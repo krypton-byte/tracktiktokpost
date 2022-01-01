edc194eeb2c84c0f4b8b1b080c8efad396c03daa
krypton-byte <rosid6434@gmail.com>
2022-01-01 19:08:36 +0700

    kebalik

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/bash
+git config user.name "github-actions"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git add .
+git commit -m 'update'
+if [ $? = 1 ]; then
+    echo "No recent Post"
+else
+    git push
+fi
