c3181eefee3c5c196543c3bc06709ec0d7a43253
krypton-byte <rosid6434@gmail.com>
2022-01-01 19:01:27 +0700

    test

--- script.sh
+++ script.sh
@@ -1,11 +1,1 @@
 #!/usr/bin/bash
-
-git config user.name "github-actions"
-git config user.email "41898282+github-actions[bot]@users.noreply.github.com""
-git add .
-git commit -m 'update'
-if [[ $? == 1 ]]; then
-    git push
-else
-    echo "No recent Post"
-fi
